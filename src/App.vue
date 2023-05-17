0573b64bbde69f32c531ac194876889dfa080373
Wes7ey <wesley7leal@outlook.com>
2023-05-17 15:14:15 -0300

    remove lógia duplicada, acrescenta links e troca informações com o router view

--- src/App.vue
+++ src/App.vue
@@ -6,60 +6,15 @@ import "@/assets/base.css"
 export default{
   data(){
     return {
-      posts:[
-        {
-        title:'Meu Primeiro Post',
-        datetime: Date.now(),
-        content: 'Postar aqui é muito legal'
-
-         },
-         {
-        title:'Meu Segundo Post',
-        datetime: Date.now(),
-        content: 'Postar aqui é muito legal'
-        },
-      ],
-      formData:{
-        title: "",
-        content: "",
-      },
-      search:"",
+      posts:[],
     };
   },
-  computed:{
-    filteredPosts() {
-
-      if(!this.search) return this.posts;
-
-      const listaFiltrada = [];
-        for (const post of this.posts){
-          if (post.title.includes(this.search)){
-            listaFiltrada.push(post);
-          }
-        }
-      return listaFiltrada;
+  methods: {
+    addPost(newPost) {
+      this.posts.push(newPost);
     },
   },
-  methods:{
-    handleCLick(event){
-  const now = new Date()
-  const dataDaPostagem=`${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`;
-
-      this.posts.push ({
-      title: this.formData.title,
-      content: this.formData.content,
-      datetime: dataDaPostagem,
-        
-    })
-  },
-
-    handleInputChange(event) {
-    const {name, value} = event.target;
-    this.formData[name] = value;
-
-      }
-    }
-  }
+ }
 
 
 
@@ -67,27 +22,12 @@ export default{
 
 <template>
   <div class="conteudo">
-<RouterView />
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/create">Novo Post</RouterLink>
+    <RouterView :posts="posts" @create-post="addPost" />
 
-<input v-model="search" placeholder="Procure pelo titulo do post..." />
+  
 
-  <div id="lista-posts">
-    <div class="post" v-for="post in filteredPosts" :key="post.key">
-    <h3>{{ post.title }}</h3>
-    <h4>{{post.datetime}}</h4>
-    <p>{{post.content}}</p>
-
-    </div>
-  </div>
-
-  <form action>
-    <input v-model="formData.title" placeholder="Título">
- 
-    <textarea name= "content" :value="formData.content" @keyup="handleInputChange" id="" cols="30" rows="10" placeholder="Escreva seu post aqui..."></textarea>
-
-    <button type="button" @click="handleCLick">Criar</button>
-
-  </form>
 
   <footer class="rodape">
       <div class="social-icons">
@@ -134,16 +74,4 @@ footer {
   display: flex;
   justify-content: center;
  }
-
-
-  form{
-  display: flex;
-  flex-direction: column;
-}
-
-form > *{
-  margin: 1rem;
-}
-
-
 </style>
