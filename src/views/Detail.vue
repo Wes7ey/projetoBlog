1b48cef4823966ec5c1a188f52a79c5236e9c753
Wes7ey <wesley7leal@outlook.com>
2023-05-22 14:54:26 -0300

    Esse commit adiciona a view de detalhemente de post

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,4 +1,37 @@
-<template>
+<script>
 
+    export default {
+        props: {
+            posts: Array,
+        },
+        data() {
+
+            const id = this.$route.params.id;
+
+            return{
+                id: id,
+                post: this.posts[id],
+            };
+        },
+        
+            
+     };
+
+</script>
+
+<template>
+<h2>{{ post.title }}</h2>
+
+<textarea readonly :value="post.content"></textarea>
 
 </template>
+
+<style scoped>
+textarea {
+    resize: none;
+    outline: none;
+    border: none;
+
+    width: 80%
+    }
+</style>
